db7a2471885988ceb76a3558d83f3f888db706a0
Latosha Tims <ltims080913@hotmail.com>
2020-05-07 09:11:30 -0700

    finished MVP and stretch

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+const server = require('./server')
+
+server.listen(5050, () => console.log('\n *** Server listening on Port 5050 *** \n'))
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
 const server = express();
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 
+server.use(logger)
+server.use(express.json())
+server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`);
+    next();
+}
 
 module.exports = server;
